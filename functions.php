2f368e297455815a524f7753992ff1e3e01eb037
Salvador Hernández Salvatierra <astrargalar@gmail.com>
2019-09-06 13:45:00 +0200

    Cambiados los tipos de letra por defecto

--- functions.php
+++ functions.php
@@ -120,11 +120,16 @@ add_action( 'widgets_init', 'mitema_widgets_init' );
  * Enqueue scripts and styles.
  */
 function mitema_scripts() {
+
+		wp_enqueue_style('mitema_fonts', 'https://fonts.googleapis.com/css?family=Dosis:300|Roboto:400,400i,700,700i&display=swap');
+
 	wp_enqueue_style( 'mitema-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'mitema-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'mitema-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'mitema-all.min', get_template_directory_uri() . '/js/all.min.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
